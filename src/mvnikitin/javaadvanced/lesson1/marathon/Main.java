c18c30485c0836befd38b2be09ef11e765bacbe4
mnikitin77 <nmax_2002@mail.ru>
2019-07-16 21:37:06 +0300

    Закомментировал исходный код и написал минимальный код для проверки класса Team.

--- src/mvnikitin/javaadvanced/lesson1/marathon/Main.java
+++ src/mvnikitin/javaadvanced/lesson1/marathon/Main.java
@@ -5,7 +5,7 @@ import mvnikitin.javaadvanced.lesson1.marathon.obstacles.*;
 
 public class Main {
     public static void main(String[] args) {
-        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
+/*        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
         Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
         for (Competitor c : competitors) {
             for (Obstacle o : course) {
@@ -15,6 +15,13 @@ public class Main {
         }
         for (Competitor c : competitors) {
             c.info();
-        }
+        } */
+
+        Team team = new Team("Трудовые резервы");
+
+        // TODO написать код соревнований
+
+        // выводим информациб о членах команды, прошедших дистанцию
+        team.showResults();
     }
 }
