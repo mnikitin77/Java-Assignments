b736e574782b1221348fdfe7f60658f47c0701b3
mnikitin77 <nmax_2002@mail.ru>
2019-07-16 21:38:26 +0300

    Почему-то не отъехал класс Team

--- src/mvnikitin/javaadvanced/lesson1/marathon/Team.java
+++ src/mvnikitin/javaadvanced/lesson1/marathon/Team.java
@@ -1,4 +1,51 @@
 package mvnikitin.javaadvanced.lesson1.marathon;
 
+import mvnikitin.javaadvanced.lesson1.marathon.participants.*;
+
 public class Team {
+
+    private String name;
+    private final static int MEMBERS_COUNT = 4;
+    private Competitor[] teamMembers;
+
+    public Team(String name) {
+        this.name = name;
+        teamMembers = new Competitor[MEMBERS_COUNT];
+
+        // Наполняем команду участниками
+        teamMembers[0] = new Human("Боб");
+        teamMembers[1] = new Cat("Барсик");
+        teamMembers[2] = new Dog("Бобик");
+        teamMembers[3] = new Human("Иван");
+
+        // Представляем команду
+        info();
+    }
+
+    /**
+     * Метод вывода информации обо всех членах команды.
+     */
+    public void info() {
+        System.out.println("Представляем участников из команды \"" + name + "\":");
+
+        for(Competitor member: teamMembers) {
+            member.info();
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * Мметод для вывода информации о членах команды, прошедших дистанцию.
+     */
+    public void showResults() {
+        System.out.println("Поздравляем наших победителей из команды \"" + name + "\":");
+
+        for(Competitor member: teamMembers) {
+            if (member.isOnDistance())
+                member.info();
+        }
+
+        System.out.println();
+    }
 }
