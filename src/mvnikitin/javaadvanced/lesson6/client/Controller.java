bc889a1c6bf3433657c08028390054cebcb1ef48
mnikitin77 <nmax_2002@mail.ru>
2019-08-02 00:06:08 +0300

    ДЗ Урока 6: добавил сервер и ГПИ сетевого чата, реализовал сетевое взаимодействие, добавил пару мелких фич.

--- src/mvnikitin/javaadvanced/lesson6/client/Controller.java
+++ src/mvnikitin/javaadvanced/lesson6/client/Controller.java
@@ -1,11 +1,20 @@
 package mvnikitin.javaadvanced.lesson6.client;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 
-public class Controller {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
     @FXML
     TextArea textArea;
 
@@ -15,13 +24,28 @@ public class Controller {
     @FXML
     Button btn1;
 
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADRESS = "localhost";
+    final int PORT = 10050;
+
+    //final String USER = "Вася";
+    final String USER = "Петя";
+
     public void sendMsg() {
         String textToSend = messageTextArea.getText();
 
         if (!textToSend.isEmpty()) {
-            textArea.appendText(textToSend + "\n");
-            messageTextArea.clear();
-            messageTextArea.requestFocus();
+            try {
+                out.writeUTF(textToSend);
+                messageTextArea.clear();
+                messageTextArea.requestFocus();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
@@ -30,5 +54,40 @@ public class Controller {
     {
         if(e.isShiftDown() && e.getCode().toString().equals("ENTER"))
             sendMsg();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                        out.writeUTF("/user:" + USER);
+
+                        while (true) {
+                            String messageReceived = in.readUTF();
+                            textArea.appendText(messageReceived + "\n");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
